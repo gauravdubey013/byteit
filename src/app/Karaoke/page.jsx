d0a53e6fc322590fcc6c4fd61f600e741873558f
Gaurav Dubey <98527111+gauravdubey013@users.noreply.github.com>
2023-11-22 12:09:20 -0800

    Update karaoke page.jsx

--- src/app/Karaoke/page.jsx
+++ src/app/Karaoke/page.jsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 const KaraokeMenu = () => {
   return (
     <>
-      <div className="w-full h-full flex flex-col gap-5 p-20">
+      <div className="w-full h-[80] flex flex-col gap-5 p-20">
         <Link href={"/Karaoke/theGoodPart"}>The Good Part - ARJ</Link>
         <Link href={"/Karaoke/theLoneliest"}>The Loneliest - Mae</Link>
       </div>
