27627073e7e29ce62107cef4e38ec42f7cd3b8f5
Gaurav Dubey <dubeygaurav520@gmail.com>
2023-12-08 08:17:34 -0800

    Update MusicPlayer.jsx

--- src/components/MusicPlayer.jsx
+++ src/components/MusicPlayer.jsx
@@ -2,7 +2,9 @@
 
 import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
+import Link from "next/link";
 import { musics } from "@/app/Karaoke/[music]/musicsdata";
+import { IoArrowBackCircleOutline } from "react-icons/io5";
 
 const MusicPlayer = (props) => {
   const titleFetch = props.setTitle;
@@ -222,10 +224,16 @@ const MusicPlayer = (props) => {
     <>
       <div className="w-full h-[71vh] md:h-screen relative px-7">
         <div className="w-auto h-[25%] md:h-[18%] md:absolute md:-ml-5 flex flex-col items-center justify-center gap-2">
+          <Link href={"/Karaoke"} className="w-[25%] h-auto flex items-center justify-center gap-2 rounded-full hover:border-[0.5px] active:scale-90">
+          <div>
+          <IoArrowBackCircleOutline />
+          </div>
+          <div>Back</div>
+          </Link>
           <h1 className="text-3xl md:text-2xl text-white font-extrabold ease-in-out duration-200">
             Play music to read lyrics...
           </h1>
-          <div className="w-full h-[30%] flex items-center justify-between gap-2 text-2xl md:text-xl text-teal-500 px-2">
+          <div className="w-full h-[30%] flex items-center justify-between gap-2 md:gap-5 text-2xl md:text-xl text-teal-500 px-2">
             <h1 className="uppercase font-bold">{titleFetch}</h1>
             <p>Points: {points}%</p>
           </div>
